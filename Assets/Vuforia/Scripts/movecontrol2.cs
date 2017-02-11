dec9a2a92fcdb0a8494d6c4e62f4bde2c7b093f6
PeterShi0629 <wenzes@andrew.cmu.edu>
2017-02-12 00:36:13 +0800

    carpediem

---
+++ Assets/Vuforia/Scripts/movecontrol2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movecontrol2 : MonoBehaviour {
+
+    // Use this for initialization
+    public spheremove1 element;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            element.startSpin();
+        }
+    }
+}
