bf0b1106ff1492b2c94badbba1ae41c9dd9ebb4a
PeterShi0629 <wenzes@andrew.cmu.edu>
2017-02-11 14:37:52 +0800

    aaa

---
+++ Assets/Vuforia/Scripts/movecontrol1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movecontrol1 : MonoBehaviour {
+
+    // Use this for initialization
+    public spheremove1 Sphere;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            Sphere.startSpin();
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameObject.transform.parent = Camera.main.transform;
+            Sphere.rightMove();
+        }
+	}
+}
---
+++ Assets/Vuforia/Scripts/spheremove1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spheremove1 : MonoBehaviour {
+
+    // Use this for initialization
+    private bool isSpinning = false;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isSpinning)
+        {
+            gameObject.transform.Rotate(new Vector3(0, Time.deltaTime*50, 0));
+        }
+	}
+
+    public void rightMove()
+    {
+        gameObject.transform.Translate(new Vector3(Time.deltaTime, 0, 0));
+    }
+
+    public void startSpin()
+    {
+        isSpinning = true;
+    }
+}
