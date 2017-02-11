db523d270553d6b24ed8de71af1412a9c8705606
PeterShi0629 <wenzes@andrew.cmu.edu>
2017-02-12 00:49:09 +0800

    asd

--- Assets/Vuforia/Scripts/spheremove1.cs
+++ Assets/Vuforia/Scripts/spheremove1.cs
@@ -6,6 +6,8 @@ public class spheremove1 : MonoBehaviour {
 
     // Use this for initialization
     private bool isSpinning = false;
+    public int speed = 50;
+
 	void Start () {
 		
 	}
@@ -14,13 +16,38 @@ public class spheremove1 : MonoBehaviour {
 	void Update () {
         if (isSpinning)
         {
-            gameObject.transform.Rotate(new Vector3(0, Time.deltaTime*50, 0));
+            gameObject.transform.Rotate(new Vector3(0, Time.deltaTime*speed, 0));
         }
 	}
 
     public void rightMove()
     {
+        gameObject.transform.Translate(new Vector3(0, 0, Time.deltaTime));
+    }
+
+    public void leftMove()
+    {
+        gameObject.transform.Translate(new Vector3(0, 0, -Time.deltaTime));
+    }
+
+    public void upMove()
+    {
+        gameObject.transform.Translate(new Vector3(0, Time.deltaTime, 0));
+    }
+
+    public void downMove()
+    {
+        gameObject.transform.Translate(new Vector3(0, -Time.deltaTime, 0));
+    }
+
+    public void frontMove()
+    {
         gameObject.transform.Translate(new Vector3(Time.deltaTime, 0, 0));
+    }
+
+    public void backMove()
+    {
+        gameObject.transform.Translate(new Vector3(-Time.deltaTime, 0, 0));
     }
 
     public void startSpin()
