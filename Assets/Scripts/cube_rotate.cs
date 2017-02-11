c19799b251b470fd8f2d0fb172c8fe6e465ea446
osguus <ovg@andrew.cmu.edu>
2017-02-11 08:46:40 -0500

    added add_earth script and carnegie imageObject

---
+++ Assets/Scripts/cube_rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(new Vector3(Time.deltaTime*30, Time.deltaTime*30, Time.deltaTime*30));
+	}
+}
---
+++ Assets/Scripts/make_sphere.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class make_sphere : MonoBehaviour {
+
+    // Public variables for inter-script communication
+    public Material mat;
+    public GameObject[] sphere_objects = new GameObject[4];
+    public GameObject self;
+
+    // Private variables for local use
+    private int count;
+
+	// Use this for initialization
+	void Start () {
+        count = 0;        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0) && count < 4)
+        {
+            createSphere();
+        }
+    }
+
+    void createSphere() {
+        sphere_objects[count] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere_objects[count].transform.SetParent(self.transform);
+        sphere_objects[count].transform.position = new Vector3(0, 0, -1/10);
+        sphere_objects[count].GetComponent<Renderer>().material.CopyPropertiesFromMaterial(mat);
+        sphere_objects[count].GetComponent<Renderer>().material.SetFloat("_Smoothness", 739/1000);
+    }
+}
