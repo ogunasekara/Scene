330bff834a6c02a2963608784d9996d46efccbdd
osguus <ovg@andrew.cmu.edu>
2017-02-11 15:52:04 -0500

    working prototype

--- Assets/Vuforia/Scripts/mobilecontrol3.cs
+++ Assets/Vuforia/Scripts/mobilecontrol3.cs
@@ -6,6 +6,7 @@ public class mobilecontrol3 : MonoBehaviour {
 
     // Use this for initialization
     public spheremove1 word;
+    public bool moving = true;
 
 	void Start () {
 		
@@ -13,9 +14,16 @@ public class mobilecontrol3 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase != TouchPhase.Moved)
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase != TouchPhase.Moved && moving)
         {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
             gameObject.transform.parent = Camera.main.transform;
+            moving = false;
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase != TouchPhase.Moved && !moving)
+        {
+            gameObject.transform.localScale = new Vector3(0, 0, 0);
+            moving = true; 
         }
     }
 }
